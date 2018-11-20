dce4492ba90c73b744528d54fcbf037219100d33
qkrgmlcjs <qkrgmlcjs5@naver.com>
2018-11-20 17:00:27 +0900

    2nd Commit - Modification

--- src/main/java/MaterialUISwingDemo.java
+++ src/main/java/MaterialUISwingDemo.java
@@ -56,26 +56,18 @@ public class MaterialUISwingDemo {
 		frame.setMinimumSize (new Dimension (600, 400));
 
 		// configuring the JMenuBar as well as its menus and items
+		// 메뉴 부분
 		JMenuBar bar = new JMenuBar ();
 		JMenu menu1 = new JMenu ("Option 1 (Animated)");
 		JMenu menu2 = new JMenu ("Option 2 (Not animated)");
 
-		JMenuItem item1 = new JMenuItem ("Item 1 (Animated)");
-		JMenuItem item2 = new JMenuItem ("Item 2 (Not animated)");
-
-		//Test RadioButtonMenuItem
-		JRadioButtonMenuItem jRadioButtonMenuItem = new JRadioButtonMenuItem ();
-		jRadioButtonMenuItem.setText ("prova RadioButtonMenuItem");
-		menu1.add (jRadioButtonMenuItem);
+		JMenuItem item1 = new JMenuItem("I don't know");
+		JMenuItem item2 = new JMenuItem("I don't know");
+		menu1.add(item1);
+		menu2.add(item2);
 		menu1.addSeparator ();
-		//TestCheckBoxMenuItem
-		JCheckBoxMenuItem checkBoxMenuItem = new JCheckBoxMenuItem ();
-		checkBoxMenuItem.setText ("test");
-		menu1.add (checkBoxMenuItem);
-		menu1.addSeparator ();
-		menu1.add (item1);
-		menu2.add (item2);
-
+		menu2.addSeparator ();
+		
 		bar.add (menu1);
 		bar.add (menu2);
 
@@ -84,9 +76,15 @@ public class MaterialUISwingDemo {
 		button.setBackground (MaterialColors.LIGHT_BLUE_400);
 		button.setForeground (Color.WHITE);
 		button.setMaximumSize (new Dimension (200, 200));
+		
+		JButton menubtn = new JButton ("ORDER");
+		menubtn.setBackground (MaterialColors.LIGHT_BLUE_400);
+		menubtn.setForeground (Color.WHITE);
+		menubtn.setMaximumSize (new Dimension (200, 200));
 
 		JPanel content = new JPanel ();
 		content.add (button);
+		content.add (menubtn);
 
 		// add everything to the frame
 		frame.add (bar, BorderLayout.PAGE_START);
@@ -95,29 +93,20 @@ public class MaterialUISwingDemo {
 		// start animating!
 		// here, 'gray' is the color that the JComponent will transition to when the user hovers over it
 		MaterialUIMovement.add (menu1, MaterialColors.GRAY_200);
-		MaterialUIMovement.add (item1, MaterialColors.GRAY_200);
+		
 
 		// you can also pass in extra parameters indicating how many intermediate colors to display, as well as the "frame rate" of the animation
 		// there will be 5 intermediate colors displayed in the transition from the original components color to the new one specified
 		// the "frame rate" of the transition will be 1000 / 30, or 30 FPS
 		// the animation will take 5 * 1000 / 30 = 166.666... milliseconds to complete
-		MaterialUIMovement.add (button, MaterialColors.LIGHT_BLUE_500, 5, 1000 / 30);
-
-		// other components
-		content.add (new JCheckBox ("checkbox"));
-		content.add (new JComboBox<String> (new String[]{"a", "b", "c"}));
-		content.add (new JLabel ("label"));
-		content.add (new JPasswordField ("password"));
-		content.add (new JRadioButton ("radio button"));
-		content.add (new JSlider (JSlider.HORIZONTAL, 0, 5, 2));
-		content.add (new JSpinner (new SpinnerListModel (new String[]{"d", "e", "f"})));
-		content.add (new JTable (new String[][]{{"a", "b", "c"}, {"d", "e", "f"}}, new String[]{"r", "e"}));
-		content.add (new JTextField ("text field"));
-		content.add (new JToggleButton ("toggle"));
-
+		MaterialUIMovement.add (button, MaterialColors.LIGHT_BLUE_600, 5, 1000 / 30);
+		MaterialUIMovement.add (menubtn, MaterialColors.LIGHT_BLUE_600, 5, 1000 / 30);
+	
+		//일단은 놔둬보자 
 		JToolBar tb = new JToolBar ("toolbar");
 		JButton button1 = new JButton ("f");
 
+		//툴바 버튼 액션이벤트리스너 
 		button1.setAction (new AbstractAction () {
 			@Override
 			public void actionPerformed (ActionEvent e) {
@@ -133,7 +122,7 @@ public class MaterialUISwingDemo {
 				dialog.pack ();
 			}
 		});
-
+		//얘도 놔둬 보자
 		JButton button2 = new JButton ("e");
 		button1.setBackground (MaterialColors.LIGHT_BLUE_400);
 		button1.setForeground (Color.WHITE);
@@ -145,51 +134,24 @@ public class MaterialUISwingDemo {
 		tb.setFloatable (true);
 		content.add (tb);
 
-		JTree tree = new JTree (new String[]{"a", "b"});
-		tree.setEditable (true);
-
-		content.add (tree);
-
 		JScrollPane sp = new JScrollPane (content);
 		sp.setHorizontalScrollBarPolicy (JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		sp.setVerticalScrollBarPolicy (JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
 		JPanel pn = new JPanel ();
 		JTabbedPane tp = new JTabbedPane ();
-		tp.addTab ("bleh", sp);
-		tp.addTab ("bleh2", pn);
+		tp.addTab ("salmon", sp);
+		tp.addTab ("chicken", pn);
 
 		frame.add (tp, BorderLayout.CENTER);
 
-		//test progressBar
-		JProgressBar progressBar = new JProgressBar ();
-		progressBar.setValue (6);
-		progressBar.setMaximum (12);
-		pn.add (progressBar);
-
-		//test cange coloro maximum value progress bar
-		progressBar = new JProgressBar ();
-		progressBar.setMaximum (5);
-		progressBar.setValue (5);
-		pn.add (progressBar);
-
-		JTextPane textPane = new JTextPane ();
-		textPane.setText ("Hi I'm super sayan");
-		JTextPane textPane1 = new JTextPane ();
-		textPane1.setText ("Hi I'm super sayan");
-		textPane1.setEnabled (false);
-		pn.add (textPane);
-		pn.add (textPane1);
-
 		JEditorPane editorPane = new JEditorPane ();
-		editorPane.setText ("This theme is fantastic");
+		editorPane.setText ("a-a");
 		pn.add (editorPane);
 
 		// make everything visible to the world
 		frame.pack ();
 		frame.setVisible (true);
 
-		JFileChooser fileChooser = new JFileChooser ();
-		fileChooser.showOpenDialog (frame);
 	}
 }
