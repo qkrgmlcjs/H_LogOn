be529f19b07f9b50a15a8c4ba6fcd6c209cb7a74
cej34567 <cej34567@naver.com>
2018-11-24 20:18:01 +0900

    for문 이용한 checkbox

--- src/main/java/Matarial.java
+++ src/main/java/Matarial.java
@@ -12,36 +12,35 @@ import mdlaf.utils.MaterialColors;
 public class Matarial extends JFrame{
 	private JLabel sumLabel;
 	private JCheckBox [] beverage = new JCheckBox [6];
-	private String [] names = {"물", "코카콜라", "스프라이트", "오렌지쥬스", "웰치스", "환타파인"};
+	private String [] beveragenames = {"물", "코카콜라", "스프라이트", "오렌지쥬스", "웰치스", "환타파인"};
+	private String [] beverageicon = {"imgs/물.jpg", "imgs/코카콜라.jpg", "imgs/스프라이트.jpg", "imgs/오렌지쥬스.jpg", "imgs/웰치스.jpg", "imgs/환타파인.jpg"};
 	
 	public void CheckBoxBeverage() {
+		JFrame frame = new JFrame ("Material Design UI for Swing by atharva washimkar");
+		frame.setMinimumSize (new Dimension (600, 400));
 		setTitle("상품주문");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		MyItemListener listener = new MyItemLister();
 		for(int i=0; i<beverage.length; i++) {
-			beverage[i] = new JCheckBox(names[i]);
-			beverage[i]
+			beverage[i] = new JCheckBox(beveragenames[i]);
+			beverage[i].setBackground (MaterialColors.LIGHT_BLUE_400);
+			beverage[i].setForeground (Color.WHITE);
+			beverage[i].setMaximumSize (new Dimension (200, 200));
+			beverage[i].setBorderPainted(true);
+			frame.add(beverage[i]);
+			beverage[i].addItemListener(listener);
 		}
+		for(int i=0; i<beverage.length; i++) {
+			ImageIcon selectedicon = new ImageIcon(beverageicon[i]);
+			beverage[i].setSelectedIcon(selectedicon);
+		}
+		sumLabel = new JLabel("현재 0원 입니다.");
+		frame.add(sumLabel);
+		setSize(250,200);
+		setVisible(true);
+	}
 		
-		// configuring a simple JButton
-				ImageIcon cokeIcon = new ImageIcon("imgs/코카콜라.jpg");
-				JCheckBox coke = new JCheckBox ("COKE", cokeIcon);
-				coke.setBackground (MaterialColors.LIGHT_BLUE_400);
-				coke.setForeground (Color.WHITE);
-				coke.setMaximumSize (new Dimension (200, 200));
-				
-				ImageIcon spriteIcon = new ImageIcon("imgs/스프라이트.jpg");
-				JButton sprite = new JButton ("Sprite");
-				sprite.setBackground (MaterialColors.LIGHT_BLUE_400);
-				sprite.setForeground (Color.WHITE);
-				sprite.setMaximumSize (new Dimension (200, 200));
-				
-				ImageIcon fantaIcon = new ImageIcon("imgs/환타파인.jpg");
-				JButton fanta = new JButton("Fanta");
-				fanta.setBackground (MaterialColors.LIGHT_BLUE_400);
-				fanta.setForeground (Color.WHITE);
-				fanta.setMaximumSize (new Dimension (100, 100));
 		
 	}
 }
