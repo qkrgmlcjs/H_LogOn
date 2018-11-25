74c857205a90330bc8ed77820d34711b8ff0a9c3
qkrgmlcjs <qkrgmlcjs5@naver.com>
2018-11-25 17:08:20 +0900

    Revert "Revert "수정 및 컴포넌트 추가""
    This reverts commit 581ef7568268b011fda9eb91e3d5966aec2c12fa.

--- src/main/java/Matarial_Snack.java
+++ src/main/java/Matarial_Snack.java
@@ -9,7 +9,7 @@ import mdlaf.utils.MaterialColors;
 
 public class Matarial_Snack {
 	public JCheckBox [] Snack_Check = new JCheckBox [6];
-	private String [] Snack_names = {"GGoggalcon", "bananaKick", "SaewooGGang", "Conchip", "Pringles", "ChipPotato"};
+	public String [] Snack_names = {"GGoggalcon", "bananaKick", "SaewooGGang", "Conchip", "Pringles", "ChipPotato"};
 	private ImageIcon [] Snack_icon = {new ImageIcon("imgs/꼬칼콘.jpg"), new ImageIcon("imgs/바나나킥.jpg"), 
 			new ImageIcon("imgs/새우깡.jpg"), new ImageIcon("imgs/콘칩.jpg"), new ImageIcon("imgs/프링글스.jpg"), 
 			new ImageIcon("imgs/칩포테토.jpg")};
@@ -20,10 +20,10 @@ public class Matarial_Snack {
 		
 		MyItemListener listener = new MyItemListener();
 		for(int i=0; i<Snack_Check.length; i++) {
-			Snack_Check[i] = new JCheckBox(Snack_names[i]);
+			Snack_Check[i] = new JCheckBox(Snack_names[i],Snack_icon[i]);
 			Snack_Check[i].setBackground (MaterialColors.LIGHT_BLUE_400);
 			Snack_Check[i].setForeground (Color.WHITE);
-			Snack_Check[i].setMaximumSize (new Dimension (200, 200));
+			//Snack_Check[i].setMaximumSize (new Dimension (200, 200));
 			Snack_Check[i].setBorderPainted(true);
 			//frame.add(beverage[i]);
 			Snack_Check[i].addItemListener(listener);
